0a25308b5a2001f0542a20456f1001124a1b1eb2
TrombiSE <38830192+TrombiSE@users.noreply.github.com>
2019-11-25 11:41:22 +0600

    DoWhile + ? Command

---
+++ doWhile.cs
@@ -0,0 +1,18 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int x = 60;
+    while (x<70){
+      Console.WriteLine(x);
+      x++;
+      //Checking on True False (If true "Yes", If false "NO")
+      string y = (x<65) ? "Yes" : "NO";
+      Console.WriteLine(y);
+      }
+    do{
+      Console.WriteLine(j);
+      j++;
+    }while (j<10);
+  }
+}
