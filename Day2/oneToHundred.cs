96037c4543023d8e7563ea7efbda1e4260831281
TrombiSE <38830192+TrombiSE@users.noreply.github.com>
2019-11-26 22:44:49 +0600

    Create oneToHundred.cs

---
+++ Day2/oneToHundred.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int sum = 0;
+    for (int x = 1; x<=100;x++){
+      sum=sum + x;
+    }
+    Console.WriteLine(sum);
+
+  }
+}
