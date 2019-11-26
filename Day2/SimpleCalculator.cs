0032305978c005075a6e6652652dd1ca6e223268
TrombiSE <38830192+TrombiSE@users.noreply.github.com>
2019-11-26 22:36:23 +0600

    Practice 1

---
+++ Day2/SimpleCalculator.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    do{
+      Console.Write("x= ");
+      int x = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("y= ");
+      int y = Convert.ToInt32(Console.ReadLine());
+
+      int sum = x + y;
+      Console.WriteLine("Result: {0}", sum);
+    }while(true);
+  }
+}
